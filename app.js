e498bf8d32b00badd3214a6c28a060e86d0184d6
Matt Gilbert <mjgila1c@gmail.com>
2018-10-06 07:50:36 +0000

    added app.js file, modified stylesheet

---
+++ app.js
@@ -0,0 +1,9 @@
+// const listItems = document.getElementsByTagName('a');
+
+// listItems.addEventListener('mouseover', () => {
+//   listItems.textContent = listItems.textContent.toUpperCase();
+// });
+
+// listItems.addEventListener('mouseout', () => {
+//   listItems.textContent = listItems.textContent.toLowerCase();
+// });
