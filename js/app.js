a9105524d024c32094d9e4bf57d509c15c1a97bc
Matt Gilbert <mjgila1c@gmail.com>
2018-10-31 09:32:53 -0400

    Updated js and jquiry into app.js and removed from HTML file. Also added play video, pause video, mute video buttons to JS and moved video into Jumbotron. Later this video will be updated to an "introduction video" explaining the purpose of this website

--- js/app.js
+++ js/app.js
@@ -21,3 +21,70 @@ const $nonscureLinks = $('a[href^="http://"]');
 
 $secureLinks.attr('target', '_blank');
 $nonscureLinks.attr('target', '_blank');
+
+// /**********************************************************************
+// **  YouTube Video Control
+//************************************************************************
+
+function openYT(){
+    $("#player").fadeIn();
+    player.playVideo();
+    $('#YTbutton').fadeOut();
+}
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+  videoId: '0i3fUOtyKa8',
+  playerVars: { 'autoplay': 0, 'controls': 0},
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange,
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  $("#YTplaybutton").fadeIn();
+  $("#YTpausebutton").fadeIn();
+  $("#YTmutebutton").fadeIn();
+  event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+  if(event.data === 0){
+    $("#player").fadeOut();
+    $("#YTplaybutton").fadeOut();
+    $("#YTpausebutton").fadeOut();
+    $("#YTbutton").fadeIn();
+  }
+}
+
+$('#YTplaybutton').on('click', function () {
+    player.playVideo();
+});
+
+$('#YTpausebutton').on('click', function () {
+    player.pauseVideo();
+});
+
+$('YTmutebutton').on('click', function() {
+    var YTmutebutton = $(this);
+
+    if(player.isMuted()){
+        player.unMute();
+        mute_toggle.text('volume_up');
+    }
+    else{
+        player.mute();
+        mute_toggle.text('volume_off');
+    }
+});
