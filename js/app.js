1b9c614e9e4c49de58eec97d89f1e1dca350551c
Matt Gilbert <mjgila1c@gmail.com>
2018-11-05 22:39:46 -0500

    added responsive buttons

--- js/app.js
+++ js/app.js
@@ -31,14 +31,14 @@ function openYT(){
     player.playVideo();
     $('#YTbutton').fadeOut();
 }
-// 2. This code loads the IFrame Player API code asynchronously.
+//This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
+// This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
