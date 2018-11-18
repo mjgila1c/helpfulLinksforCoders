75ba4a423d0bf24db8b4231f3f058ee80a14a7ce
Matt Gilbert <qwk8hqx@ups.com>
2018-11-18 07:47:27 +0000

    added Facebook Like button

--- js/app.js
+++ js/app.js
@@ -112,3 +112,15 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+//*****************************************************************************
+//                Facebook Like Button
+//*****************************************************************************/
+
+ (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2';
+  fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
